ae0ba228841cd263a313533e6df11989b4dcb942
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2017-09-04 23:24:08 -0300

    MAE61 Added MessageRouter and RPC

---
+++ MessageRouter.go
@@ -0,0 +1,39 @@
+package gohelprabbitmq
+
+import (
+	"github.com/Jeffail/gabs"
+)
+
+// MessageRouter routes the message through a rabbitmq connection given a primary forward to path or return an existing RPC call
+type MessageRouter struct {
+	forwardToPath string
+	connection    *Connection
+}
+
+// NewMessageRouter creates a new MessageRouter
+func NewMessageRouter(forwardToPath string, connection *Connection) (messageRouter *MessageRouter) {
+	return &MessageRouter{
+		forwardToPath,
+		connection,
+	}
+}
+
+// Route will figute out the container's next path and route it accordingly
+func (messageRouter *MessageRouter) Route(container *gabs.Container) (err error) {
+	path, err := messageRouter.popRoutePath(container)
+	if err != nil {
+		return
+	}
+	publisher := NewSimplePublisher(messageRouter.connection, path)
+	return publisher.Publish(container.Bytes())
+}
+
+func (messageRouter *MessageRouter) popRoutePath(container *gabs.Container) (routePath string, err error) {
+	if container.ExistsP(messageRouter.forwardToPath) == false {
+		return popRPCQueue(getRPCContainerParent(container))
+	}
+
+	routePath = container.Path(messageRouter.forwardToPath).Data().(string)
+	container.DeleteP(messageRouter.forwardToPath)
+	return
+}
---
+++ RPC_test.go
@@ -0,0 +1,117 @@
+package gohelprabbitmq
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/Cappta/gofixture"
+	"github.com/Jeffail/gabs"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	rabbitMQ             = ConnectLocallyWithDefaultUser()
+	testServiceConsumer  = NewAsyncConsumer(rabbitMQ, "TestForRPC")
+	testServicePublisher = NewSimplePublisher(rabbitMQ, fmt.Sprintf("@%s", testServiceConsumer.QueueSettings.GetName()))
+	rpc                  = NewRPC(NewSimpleConsumer(rabbitMQ, "CallbackToRPC"), time.Second*5)
+	messageRouter        = NewMessageRouter("TestForRPC.ForwardTo", rabbitMQ)
+)
+
+func TestRPC(t *testing.T) {
+	done := make(chan bool)
+	go func() {
+		testServiceConsumer.Consume()
+		done <- true
+	}()
+	go rpc.Consume()
+
+	err := NewQueueObserver(rabbitMQ, testServiceConsumer.QueueSettings).AwaitConsumer(time.Second * 5)
+	if err != nil {
+		panic(err)
+	}
+	err = NewQueueObserver(rabbitMQ, rpc.QueueSettings).AwaitConsumer(time.Second * 5)
+	if err != nil {
+		panic(err)
+	}
+
+	Convey("Given a running async consumer", t, func() {
+		Convey("Given any string with length between 10 and 100", func() {
+			anyString := AnyString(AnyIntBetween(10, 100))
+			Convey("Given a json container with the string set as value", func() {
+				container := gabs.New()
+				path := "Value"
+				container.SetP(anyString, path)
+				Convey("When preparing the rpc", func() {
+					callback := rpc.Prepare(container)
+					Convey("Then callback should not be nil", func() {
+						So(callback, ShouldNotBeNil)
+						Convey("When publishing to the test service", func() {
+							testServicePublisher.Publish(container.Bytes())
+							Convey("Then the test service should receive the message within five seconds", func() {
+								message, err := testServiceConsumer.AwaitDelivery(time.Second * 5)
+								Convey("Then error should be nil", func() {
+									So(err, ShouldBeNil)
+									Convey("Then message should not be nil", func() {
+										So(message, ShouldNotBeNil)
+										Convey("Then message should be a valid json", func() {
+											messageContainer, err := gabs.ParseJSON(message.Body)
+											So(err, ShouldBeNil)
+											Convey("Then value's path should exist", func() {
+												So(messageContainer.ExistsP(path), ShouldBeTrue)
+												Convey("Then returned value should equal provided value", func() {
+													So(messageContainer.Path(path).Data().(string), ShouldEqual, anyString)
+													Convey("When routing the message", func() {
+														err = messageRouter.Route(messageContainer)
+														Convey("Then error should be nil", func() {
+															So(err, ShouldBeNil)
+															Convey("Then rpc should receive the message within 5 seconds", func() {
+																select {
+																case <-time.After(time.Second * 5):
+																	t.Fail()
+																case returnedMessage := <-callback:
+																	Convey("Then message should not be nil", func() {
+																		So(returnedMessage, ShouldNotBeNil)
+																		Convey("Then message should be a valid json", func() {
+																			returnedMessageContainer, err := gabs.ParseJSON(returnedMessage.Body)
+																			So(err, ShouldBeNil)
+																			Convey("Then value's path should exist", func() {
+																				So(returnedMessageContainer.ExistsP(path), ShouldBeTrue)
+																				Convey("Then returned value should equal provided value", func() {
+																					So(returnedMessageContainer.Path(path).Data().(string), ShouldEqual, anyString)
+																					Convey("Then RPC's path should not exist", func() {
+																						fmt.Println(":" + returnedMessageContainer.String())
+																						So(returnedMessageContainer.Exists(rpcPath), ShouldBeFalse)
+																					})
+																				})
+																			})
+																		})
+																	})
+																}
+															})
+														})
+													})
+												})
+											})
+										})
+									})
+								})
+							})
+						})
+					})
+				})
+			})
+		})
+	})
+
+	err = testServiceConsumer.StopConsuming()
+	if err != nil {
+		panic(err)
+	}
+	err = rpc.StopConsuming()
+	if err != nil {
+		panic(err)
+	}
+	<-done
+}
